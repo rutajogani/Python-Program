d660c4180678aa2e9bf42f8544f263c0f1d8118b
Ruta Jogani <ruta007jogani@gmail.com>
2026-01-05 23:21:26 +0530

    Add: code in armstrong number

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+def checkArmstrong(number):
+    sum = 0
+    ans = number
+
+    while number > 0:
+        singleDigit = number % 10
+        sum += singleDigit * singleDigit * singleDigit
+        number = number / 10
+
+    if ans == sum:
+        print("This Number Is Armstrong")
+    else:
+        print("This Number Is Normal")
+
+
+print(checkArmstrong(153))
