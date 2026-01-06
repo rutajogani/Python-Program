578ec85f6b73b0c61bfc26179c280b6a7991ea1e
Ruta Jogani <ruta007jogani@gmail.com>
2026-01-06 22:23:36 +0530

    change same code

--- armstrong.py
+++ armstrong.py
@@ -1,18 +1,16 @@
-# # Armstrong Number
+# Armstrong Number
 
-def checkArmstrong(number):
-    sum = 0
-    ans = number
+number = int(input('Enter Number: '))
+sum = 0
+ans = number
 
-    while number > 0:
-        singleDigit = number % 10
-        sum += singleDigit * singleDigit * singleDigit
-        number = number / 10
+while number != 0:
+    singleDigit = number % 10
+    sum += singleDigit * singleDigit * singleDigit
+    number = number / 10
 
-    if ans == sum:
-        print("This Number Is Armstrong")
-    else:
-        print("This Number Is Normal")
+if ans == sum:
+    print("This Number Is Armstrong")
+else:
+    print("This Number Is Normal")
 
-
-print(checkArmstrong(153))
