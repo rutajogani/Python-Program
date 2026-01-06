8ee9de9b790ff931e5e29c2dea9b87c1c57f3fc5
Ruta Jogani <ruta007jogani@gmail.com>
2026-01-06 22:54:22 +0530

    some change in armstrong

--- armstrong.py
+++ armstrong.py
@@ -4,7 +4,7 @@ number = int(input('Enter Number: '))
 sum = 0
 ans = number
 
-while number != 0:
+while number > 0:
     singleDigit = number % 10
     sum += singleDigit * singleDigit * singleDigit
     number = number / 10
