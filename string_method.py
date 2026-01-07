4debeb512c9fb0664132e7c76e0d4a57b040fd63
Ruta Jogani <ruta007jogani@gmail.com>
2026-01-07 22:41:54 +0530

    add line update code in string method

--- string_method.py
+++ string_method.py
@@ -1,8 +1,12 @@
 string = """my favorite car is bmw.
 my favorite fruit is mango.
 my favorite game is badminton."""
+
 txt = "hello, world!"
+
 my_tuple = ("apple","banana","mango")
+
+new = "HELLO, GUYS!"
 
 #length of string
 print(len(string))
@@ -22,3 +26,29 @@ print("--------------------------")
 c = ",".join(my_tuple)
 print(c)
 print("--------------------------")
+
+#replace string method
+d = string.replace("my","jon's")
+print(d)
+print("--------------------------")
+
+#lower method with string
+e = new.lower()
+print(e)
+print("--------------------------")
+
+#upper method with string
+f = txt.upper()
+print(f)
+print("--------------------------")
+
+#minimum method with string
+g = [5, 10, 25, 2]
+print(min(g))
+print("--------------------------")
+
+#maximum method with string
+h = [5, 10, 25, 2]
+print(max(h))
+print("--------------------------")
+
